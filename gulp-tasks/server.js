abdd49bdd54554d06d17cc24d12c2540e058415c
rtxrulez <vafarulez@ya.ru>
2018-01-30 22:19:41 +0300

    сервер

--- gulp-tasks/server.js
+++ gulp-tasks/server.js
@@ -2,13 +2,13 @@ const config = require("./config");
 const gulp = require("gulp");
 const serv = require("gulp-server-livereload");
 
-gulp.task("webserver", function(cb) {
+gulp.task("server", function(cb) {
   gulp.src(config.path.dist.pages).pipe(
     serv({
       livereload: true,
       port: "3005",
-      directoryListing: false,
-      log: "debug",
+      directoryListing: true,
+    //   log: "debug",
       clientConsole: true
     })
   );
