93a4c1f679216e41bbf220ea58072a7b0f7832e5
rtxrulez <vafarulez@ya.ru>
2018-02-18 23:03:32 +0300

    autoprefixer

--- gulp-tasks/styles.js
+++ gulp-tasks/styles.js
@@ -1,11 +1,26 @@
-const config = require("./config");
-const gulp = require("gulp");
-const sass = require("gulp-sass");
-const plumber = require("gulp-plumber");
+const config       = require("./config");
+const gulp         = require("gulp");
+const sass         = require("gulp-sass");
+const plumber      = require("gulp-plumber");
+const postcss      = require('gulp-postcss');
+const sourcemaps   = require('gulp-sourcemaps');
+const autoprefixer = require('autoprefixer');
+const livereload   = require("gulp-livereload");
+const gulpIf       = require("gulp-if");
+
+// Настройки postcss
+const postcssPlugins = [ 
+  require('postcss-pseudo-class-enter'),
+  autoprefixer()
+]
+// режим разработки?
+const dev = !process.env.NODE_ENV || process.env.NODE_ENV == "dev";
+
 
 gulp.task("styles", function() {
   return gulp
     .src([config.path.styles + "*.scss", config.path.styles + "*.sass"])
+    .pipe(gulpIf(dev, sourcemaps.init()))
     .pipe(
       plumber({
         errorHandler: function(error) {
@@ -15,5 +30,8 @@ gulp.task("styles", function() {
       })
     )
     .pipe(sass())
+    .pipe(postcss(postcssPlugins))
+    .pipe(gulpIf(dev, sourcemaps.write('.')))
+    .pipe(gulpIf(dev, livereload()))
     .pipe(gulp.dest(config.path.dist.styles));
 });
