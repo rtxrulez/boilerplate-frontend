f914f3431f8fb0e7f02bf689ba8567d78f9af4a0
rtxrulez <vafarulez@ya.ru>
2018-02-25 20:44:01 +0300

    стили

--- gulp-tasks/styles.js
+++ gulp-tasks/styles.js
@@ -17,13 +17,14 @@ const dev = !process.env.NODE_ENV || process.env.NODE_ENV == "dev";
 
 // Настройки postcss
 const postcssPlugins = [
-  require('precss')({
-    import: {
-      extension: 'scss'
-    }
-  }), // транспилинг scss 
+  // require('precss')({
+  //   import: {
+  //     extension: 'scss'
+  //   }
+  // }), // транспилинг scss 
+  // require('postcss-scss'),
   require("css-mqpacker"), // группирует медиа выражения
-  require('postcss-pseudo-class-enter'), // позволяет писать один псевдокласс для :focus и :hover
+  // require('postcss-pseudo-class-enter'), // позволяет писать один псевдокласс для :focus и :hover
   autoprefixer(),
 ];
 
@@ -53,7 +54,7 @@ gulp.task("styles", function (cb) {
         }
       })
     )
-    // .pipe(sass())
+    .pipe(sass())
 
     .pipe(postcss(postcssPlugins))
     // .pipe(gulpIf(!dev, postcss([ // Если продакшен то сжимаем стили 
