4f8c6cb1a42596d4a0192d356ec0479e2b0ffb51
rtxrulez <vafarulez@ya.ru>
2018-04-07 12:27:17 +0300

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,10 @@
 const webpack = require('webpack');
 const path = require('path')
 const config = require("./gulp-tasks/config");
+
 // режим разработки?
 const NODE_ENV = process.env.NODE_ENV ? process.env.NODE_ENV.trim() : 'dev'; // trim для удаления лишних пробелов, если платформа Windows
-console.log('dddddddddd', NODE_ENV)
-console.log('tt', path.join(__dirname, config.path.scripts, 'app.js'))
 
-
-const nodeModulesPath = path.join(__dirname, "node_modules");
-// const dev = !process.env.NODE_ENV || process.env.NODE_ENV == "dev";
 module.exports = {
     context: path.join(__dirname, config.path.scripts),
     entry: {
@@ -27,10 +23,9 @@ module.exports = {
             'jQuery': 'jquery',
             'jquery': 'jquery',
         },
-        modules: [
-            "node_modules",
-            path.resolve(__dirname, "app")
-        ],
+        module: {
+            
+        }
         // modules: ['node_modules'],
         extensions: [".js", ".json", ".jsx", ".css"],
     }
